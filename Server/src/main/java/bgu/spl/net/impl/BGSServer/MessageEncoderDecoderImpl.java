9e3a37139d3688a076c346b0632ef5abf3e0b866
niryaron66 <95036279+niryaron66@users.noreply.github.com>
2022-11-03 12:32:49 +0200

    Update MessageEncoderDecoderImpl.java

--- Server/src/main/java/bgu/spl/net/impl/BGSServer/MessageEncoderDecoderImpl.java
+++ Server/src/main/java/bgu/spl/net/impl/BGSServer/MessageEncoderDecoderImpl.java
@@ -7,7 +7,6 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-//Should be T or not??????????
 public class MessageEncoderDecoderImpl implements MessageEncoderDecoder<Message> {
     private short opcode;
     private int len = 0;
